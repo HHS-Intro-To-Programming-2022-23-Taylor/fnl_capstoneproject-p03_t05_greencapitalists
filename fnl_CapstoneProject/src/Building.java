2fbec6563b0901efd27c167743b37a5030ed50ae
Dhruv Jain <djain859@student.fuhsd.org>
2023-05-02 13:26:04 -0700

    Building Commit
    Made more methods in building classes.

--- fnl_CapstoneProject/src/Building.java
+++ fnl_CapstoneProject/src/Building.java
@@ -1,4 +1,52 @@
+/*
+ * Author : Dhruv Jain
+ * Date   : 5/2/23
+ * Rev    : 1
+ * Notes  : 
+ */
 
-public class Building {
+import java.awt.Image;
 
-}
+ public abstract class Building {
+	//Fields
+	private int enviScoreDecrease;
+    private int points;
+    private Image unsustainable;
+    private Image sustainable;
+    
+	
+	//Constructor
+	
+	//Methods
+    
+    //Returns the environmental score decrease.
+    public int getEnviScore()
+    {
+    	return enviScoreDecrease;
+    }
+
+    //Returns the image for the sustainable building.
+    public Image getSustainable()
+    {
+    	return sustainable;
+    }
+    
+    //Returns the image for the sustainable building. 
+    public Image getUnsustainable()
+    {
+    	return unsustainable;
+    }
+
+    //Returns the point value.
+    public int getPoints()
+
+    
+    {
+    	return points;
+    }
+
+    public String getType() {
+    	return "";
+    }
+
+ }
