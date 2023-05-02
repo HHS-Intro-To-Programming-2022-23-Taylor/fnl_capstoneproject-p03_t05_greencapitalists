b41885603f4b0cec9f87c1649085049e638b69af
Sanah Bhandari <112981031+BhandariSanah@users.noreply.github.com>
2023-05-02 13:21:36 -0700

    Create Draw.java

---
+++ fnl_CapstoneProject/src/Draw.java
@@ -0,0 +1,30 @@
+// Author: Sanah Bhandari
+// Date  : May 2, 2023
+// Rev   : 01
+// Notes : Notes go here
+
+import java.awt.Graphics;
+import java.awt.*;
+import javax.swing.*;
+
+@SuppressWarnings("serial")
+public class Draw extends JPanel {
+
+	JPanel panel;
+	JFrame score;
+	JFrame points;
+	JFrame resources;
+	JButton buyBuilding1;
+	JButton buyBuilding2;
+	JButton buyBuilding3;
+	JButton buyBuilding4;
+	
+	public void paint(Graphics g) {
+		   
+	      super.paintComponent(g);
+	      
+	      g.setColor(Color.GRAY);
+	      
+	}
+	
+}
