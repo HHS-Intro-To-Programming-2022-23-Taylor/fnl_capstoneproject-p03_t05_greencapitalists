7125fa7fd8864e522a7aa11377dc6fe571f3d9e3
Dhruv Jain <djain859@student.fuhsd.org>
2023-05-04 13:02:15 -0700

    Add ResourceBuilding class
    Made Building Abstract. ResourceBuilding will also be abstract. There will be 4 classes, extending Building OR Building/ResourceBuilding both, based on their type.

---
+++ fnl_CapstoneProject/src/ResourceBuilding.java
@@ -0,0 +1,18 @@
+import java.awt.Image;
+
+import javax.swing.ImageIcon;
+
+
+public class ResourceBuilding extends Building{
+
+//	Fields - Just a reference on what fields are needed
+	
+	private int points;
+	private Image img2;
+	private Image houseImage
+	private int cost;
+	
+	
+	
+
+}
