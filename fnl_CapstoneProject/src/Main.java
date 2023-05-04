d1e475f34fe8c63caa6a46c1ea43bbf4635c6f3c
Sanah Bhandari <112981031+BhandariSanah@users.noreply.github.com>
2023-05-04 12:28:46 -0700

    Created Window
    Main Window made in Main.

--- fnl_CapstoneProject/src/Main.java
+++ fnl_CapstoneProject/src/Main.java
@@ -1,10 +1,21 @@
+import java.awt.*;
+import javax.swing.*;
 
-
-public class Main {
+public class Main extends JPanel {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+		// create a window frame
+	    JFrame mainScreen = new JFrame ("Main Screen");
+	    mainScreen.setBounds(300, 300, 800, 600);   //establish initial size in pixels
+	    mainScreen.getContentPane().setBackground(Color.WHITE); // set background color
 
+	    mainScreen.setResizable(false); // the window can be resized
+	    mainScreen.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // this happens with window is closed
+	    mainScreen.setVisible(true);
+		
+		
 	}
 
 }
