666cd645c54bb8028f89f5c5e9e5271de77ab958
Sanah Bhandari <112981031+BhandariSanah@users.noreply.github.com>
2023-05-09 13:13:31 -0700

    Worked on Draw
    Buttons are added, calls the resourcemanagment buy house method for the buy housebutton

--- fnl_CapstoneProject/src/Draw.java
+++ fnl_CapstoneProject/src/Draw.java
@@ -4,24 +4,44 @@
 // Notes : Notes go here
 
 import java.awt.*;
+import java.awt.event.*;
 import javax.swing.*;
 
 @SuppressWarnings("serial")
-public class Draw extends JPanel {
+public class Draw extends JPanel implements ActionListener {
 
 	// JPanel panel;
 	// JFrame score;
 	// JFrame points;
 	// JFrame resources;
-	// JButton buyBuilding1;
-	// JButton buyBuilding2;
-	// JButton buyBuilding3;
-	// JButton buyBuilding4;
+	JButton buyHouse, buyStore, buyFactory, buySolarPanel;
 	// JTextField display = new JTextField("hi");
 	private Image buildings = new ImageIcon("buildings.gif").getImage();
-	private Image house = new ImageIcon("House.gif").getImage();
+	private ImageIcon house = new ImageIcon("House.gif");
 	ResourceManagement resourceManager = new ResourceManagement();
 
+	public Draw() {
+		
+		buyHouse = new JButton(" 25 ", house);
+	    buyHouse.addActionListener(this);
+		buyStore = new JButton(" 10 ", house);
+		buyStore.addActionListener(this);
+		buyFactory = new JButton("  5  ", house);
+		buyFactory.addActionListener(this);
+		buySolarPanel = new JButton("  5 ", house);
+		buySolarPanel.addActionListener(this);
+		
+		JPanel buttons = new JPanel(new GridLayout(10, 20, 20, 5));
+		buttons.setBackground(Color.BLUE);
+		buttons.add(buyHouse);
+//		buttons.add(buyStore);
+//		buttons.add(buyFactory);
+//		buttons.add(buySolarPanel);
+		
+		this.add(buttons);
+		
+	}
+	
 	public void paint(Graphics g) {
 
 		super.paintComponent(g);
@@ -38,7 +58,7 @@ public class Draw extends JPanel {
 		g.drawRect(50, 100, 200, 400);
 		g.drawString("Build House:", 70, 120);
 		g.drawString("resources", 70, 140);
-		g.drawImage(house, 200, 100, 50, 50, this);
+		//g.drawImage(house, 200, 100, 50, 50, this);
 
 		// build stores box
 		g.drawRect(50, 100, 200, 300);
@@ -63,13 +83,20 @@ public class Draw extends JPanel {
 		// draws buildings image in screen's center
 		g.drawImage(buildings, 300, 100, 400, 350, this);
 
+		// buttons.draw(g);
 	}
 
-	//	public void reload()
-	//	{
-	//		// vendor.setResources(FULL_STOCK);
-	//		// display.setText(" " + vendor.getDeposit() + "  ");
-	//		repaint();
-	//	}
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		JButton b = (JButton)e.getSource();
+		
+		if (b == buyHouse)
+		{
+			resourceManager.buyHouse();
+		}
+	}
+
+
 
 }
