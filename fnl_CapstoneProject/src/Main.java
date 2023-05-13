f612141e5a05dd709d3360195490aed91fa38a88
Dhruv Jain <djain859@student.fuhsd.org>
2023-05-12 18:45:28 -0700

    Merged Draw and Main's functions, got MouseListener to work.
    All of main's functionality has been moved to Draw class, all main does now is Instantiate a Draw.
    
    Mouse listener now works, wherever you click, the coords will be given.

--- fnl_CapstoneProject/src/Main.java
+++ fnl_CapstoneProject/src/Main.java
@@ -12,26 +12,27 @@ public class Main {
 		// TODO Auto-generated method stub
 
 		// create a window frame
-		JFrame mainScreen = new JFrame ("Main Screen");
-		mainScreen.setBounds(300, 100, 800, 600);   //establish initial size in pixels
-		mainScreen.getContentPane().setBackground(Color.WHITE); // set background color
-
-
-		mainScreen.setResizable(false); // the window can be resized
-		mainScreen.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // this happens with window is closed
-		mainScreen.setVisible(true);
-
-		// create components (widgets) to add to the window.  this can be panels, buttons, images, etc.
-		// in this case a new panel     
-		Draw mainPanel = new Draw(); // create a panel to add to window
-		mainPanel.setBackground(Color.WHITE);// set the background color of the panel to white
-
-		// add the panel to the frame
-		Container windowContainer = mainScreen.getContentPane();// get the window's container
-		windowContainer.add(mainPanel); // add the panel to the window
-
-		// make the window visible
-		mainScreen.setVisible(true);
+		Draw panel = new Draw();
+//		JFrame mainScreen = new JFrame ("Main Screen");
+//		mainScreen.setBounds(300, 100, 800, 600);   //establish initial size in pixels
+//		mainScreen.getContentPane().setBackground(Color.WHITE); // set background color
+//
+//
+//		mainScreen.setResizable(false); // the window can be resized
+//		mainScreen.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // this happens with window is closed
+//		mainScreen.setVisible(true);
+//
+//		// create components (widgets) to add to the window.  this can be panels, buttons, images, etc.
+//		// in this case a new panel     
+//		Draw mainPanel = new Draw(); // create a panel to add to window
+//		mainPanel.setBackground(Color.WHITE);// set the background color of the panel to white
+//
+//		// add the panel to the frame
+//		Container windowContainer = mainScreen.getContentPane();// get the window's container
+//		windowContainer.add(mainPanel); // add the panel to the window
+//
+//		// make the window visible
+//		mainScreen.setVisible(true);
 
 	}
 
